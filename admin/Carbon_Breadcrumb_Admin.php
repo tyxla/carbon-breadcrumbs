fd8271f7ec4c83d10db63972a5f25a040f927e96
Marin Atanasov <tyxla@abv.bg>
2015-01-30 01:34:36 +0200

    starting admin

---
+++ admin/Carbon_Breadcrumb_Admin.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * The main breadcrumb administration class.
+ * 
+ * Includes, wraps and manages the administration functionality.
+ */
+class Carbon_Breadcrumb_Admin {
+
+	/**
+	 * Constructor.
+	 *
+	 * Creates the administration functionality wrapper.
+	 *
+	 * @access public
+	 *
+	 * @return Carbon_Breadcrumb_Admin
+	 */
+	public function __construct() {
+
+
+	
+	}
+	
+}
