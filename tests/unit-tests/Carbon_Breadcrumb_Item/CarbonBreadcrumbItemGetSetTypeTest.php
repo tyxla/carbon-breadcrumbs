3805c14101f5957fd9fa7f7138a862cc725dc6b9
Marin Atanasov <tyxla@abv.bg>
2015-11-02 22:20:35 +0200

    general tests for Carbon_Breadcrumb_Item::get_type() and Carbon_Breadcrumb_Item::set_type()

---
+++ tests/unit-tests/Carbon_Breadcrumb_Item/CarbonBreadcrumbItemGetSetTypeTest.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @group item
+ */
+class CarbonBreadcrumbItemGetSetTypeTest extends WP_UnitTestCase {
+
+	public function setUp() {
+		$this->item = $this->getMockForAbstractClass('Carbon_Breadcrumb_Item');
+	}
+
+	public function tearDown() {
+		unset( $this->item );
+	}
+
+	/**
+	 * @covers Carbon_Breadcrumb_Item::get_type
+	 * @covers Carbon_Breadcrumb_Item::set_type
+	 */
+	public function testGetSetType() {
+		$expected = 'fooBar';
+		$this->item->set_type( $expected );
+		$this->assertSame( $expected, $this->item->get_type() );
+	}
+
+}
