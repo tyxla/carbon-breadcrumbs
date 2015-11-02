d735e3eff1eefa9bf7f7d472bfd64b06b3d3500a
Marin Atanasov <tyxla@abv.bg>
2015-11-02 22:07:42 +0200

    general tests for Carbon_Breadcrumb_Item::get_title() and Carbon_Breadcrumb_Item::set_title()

---
+++ tests/unit-tests/Carbon_Breadcrumb_Item/CarbonBreadcrumbItemGetSetTitleTest.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @group item
+ */
+class CarbonBreadcrumbItemGetSetTitleTest extends WP_UnitTestCase {
+
+	public function setUp() {
+		$this->item = $this->getMockForAbstractClass('Carbon_Breadcrumb_Item');
+	}
+
+	public function tearDown() {
+		unset( $this->item );
+	}
+
+	/**
+	 * @covers Carbon_Breadcrumb_Item::get_title
+	 * @covers Carbon_Breadcrumb_Item::set_title
+	 */
+	public function testGetSetItems() {
+		$expected = 'Foo Bar';
+		$this->item->set_title( $expected );
+		$this->assertSame( $expected, $this->item->get_title() );
+	}
+
+}
