960734da4b6aa3499435f30c7e56dc056e0da3d6
Marin Atanasov <tyxla@abv.bg>
2015-11-02 22:21:40 +0200

    general tests for Carbon_Breadcrumb_Item::get_subtype() and Carbon_Breadcrumb_Item::set_subtype()

---
+++ tests/unit-tests/Carbon_Breadcrumb_Item/CarbonBreadcrumbItemGetSetSubtypeTest.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @group item
+ */
+class CarbonBreadcrumbItemGetSetSubtypeTest extends WP_UnitTestCase {
+
+	public function setUp() {
+		$this->item = $this->getMockForAbstractClass('Carbon_Breadcrumb_Item');
+	}
+
+	public function tearDown() {
+		unset( $this->item );
+	}
+
+	/**
+	 * @covers Carbon_Breadcrumb_Item::get_subtype
+	 * @covers Carbon_Breadcrumb_Item::set_subtype
+	 */
+	public function testGetSetSubtype() {
+		$expected = 'foo_bar';
+		$this->item->set_subtype( $expected );
+		$this->assertSame( $expected, $this->item->get_subtype() );
+	}
+
+}
