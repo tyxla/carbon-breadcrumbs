800455d3dbc9f5e5f8e10a54728e2ad716b2e64d
Marin Atanasov <tyxla@abv.bg>
2015-11-02 22:04:54 +0200

    general tests for Carbon_Breadcrumb_Trail::get_items() and Carbon_Breadcrumb_Trail::set_items()

---
+++ tests/unit-tests/Carbon_Breadcrumb_Trail/CarbonBreadcrumbTrailGetSetItemsTest.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @group trail
+ */
+class CarbonBreadcrumbTrailGetSetItemsTest extends WP_UnitTestCase {
+
+	public function setUp() {
+		$this->trail = $this->getMock('Carbon_Breadcrumb_Trail', null);
+		$this->item = $this->getMockForAbstractClass('Carbon_Breadcrumb_Item');
+	}
+
+	public function tearDown() {
+		unset( $this->trail );
+		unset( $this->item );
+	}
+
+	/**
+	 * @covers Carbon_Breadcrumb_Trail::get_items
+	 * @covers Carbon_Breadcrumb_Trail::set_items
+	 */
+	public function testGetSetItems() {
+		$expected = array( $this->item );
+		$this->trail->set_items( $expected );
+		$this->assertSame( $expected, $this->trail->get_items() );
+	}
+
+}
