23203686181fbbac890a5f57f2cd477af354699d
Marin Atanasov <tyxla@abv.bg>
2015-11-02 22:30:50 +0200

    general tests for Carbon_Breadcrumb_Locator::get_type() and Carbon_Breadcrumb_Locator::set_type()

---
+++ tests/unit-tests/Carbon_Breadcrumb_Locator/CarbonBreadcrumbLocatorGetSetTypeTest.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @group locator
+ */
+class CarbonBreadcrumbLocatorGetSetTypeTest extends WP_UnitTestCase {
+
+	public function setUp() {
+		$this->locator = $this->getMockForAbstractClass( 'Carbon_Breadcrumb_Locator', array( 'test1', 'test2' ) );
+	}
+
+	public function tearDown() {
+		unset( $this->locator );
+	}
+
+	/**
+	 * @covers Carbon_Breadcrumb_Locator::get_type
+	 * @covers Carbon_Breadcrumb_Locator::set_type
+	 */
+	public function testGetSetType() {
+		$expected = 'fooBar';
+		$this->locator->set_type( $expected );
+		$this->assertSame( $expected, $this->locator->get_type() );
+	}
+
+}
