5cc8a1392b92d3359d2244672d14b4cad5b6398c
Marin Atanasov <tyxla@abv.bg>
2015-11-05 01:09:07 +0200

    minor fix

--- admin/Carbon_Breadcrumb_Admin_Settings_Field.php
+++ admin/Carbon_Breadcrumb_Admin_Settings_Field.php
@@ -67,7 +67,7 @@ abstract class Carbon_Breadcrumb_Admin_Settings_Field extends Carbon_Breadcrumb_
 	 * @return Carbon_Breadcrumb_Admin_Settings_Field $field
 	 */
 	public static function factory( $type, $id, $title, $section = '', $args = array() ) {
-		$class = self::build_class_name( __CLASS__, $type, 'Unknown settings field type "' . $type . '".' );
+		$class = self::verify_class_name( __CLASS__ . '_' . $type, 'Unknown settings field type "' . $type . '".' );
 		$field = new $class( $id, $title, $section, $args );
 
 		return $field;
