5781108de3656d4110d98eea5f563a360c7695c9
Marin Atanasov <tyxla@abv.bg>
2015-11-05 01:23:41 +0200

    general tests for Carbon_Breadcrumb_Locator::__construct()

---
+++ tests/unit-tests/Carbon_Breadcrumb_Locator/CarbonBreadcrumbLocatorConstructTest.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @group locator
+ */
+class CarbonBreadcrumbLocatorConstructTest extends WP_UnitTestCase {
+
+	public function setUp() {
+		$this->locator = $this->getMockForAbstractClass('Carbon_Breadcrumb_Locator', array(), '', false);
+	}
+
+	public function tearDown() {
+		unset( $this->locator );
+	}
+
+	/**
+	 * @covers Carbon_Breadcrumb_Locator::__construct
+	 */
+	public function testConstructor() {
+		$this->locator->__construct('foo', 'bar');
+
+		$this->assertSame( 'foo', $this->locator->get_type() );
+		$this->assertSame( 'bar', $this->locator->get_subtype() );
+	}
+
+}
