3a2b5de75dab8b63dc67c85e01d92f6af4e7048b
Marin Atanasov <tyxla@abv.bg>
2015-11-08 00:21:31 +0200

    tests for Carbon_Breadcrumb_Item_Custom::setup()

---
+++ tests/unit-tests/Carbon_Breadcrumb_Item_Custom/CarbonBreadcrumbItemCustomSetupTest.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @group item
+ */
+class CarbonBreadcrumbItemCustomSetupTest extends WP_UnitTestCase {
+
+	public function setUp() {
+		$this->item = $this->getMockForAbstractClass('Carbon_Breadcrumb_Item_Custom', array(), '', false);
+	}
+
+	public function tearDown() {
+		unset( $this->item );
+	}
+
+	/**
+	 * @covers Carbon_Breadcrumb_Item_Custom::setup()
+	 */
+	public function testConstructor() {
+		$item_clone_a = clone $this->item;
+		$item_clone_b = clone $this->item;
+		$item_clone_a->setup();
+
+		$expected = serialize( $item_clone_a );
+		$actual = serialize( $item_clone_b );
+		$this->assertSame( $expected, $actual );
+	}
+
+}
