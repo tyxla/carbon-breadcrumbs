a8b357e83d1bbc698403eb499612aec9717dc7fb
Marin Atanasov <tyxla@abv.bg>
2015-11-10 23:16:13 +0200

    Carbon_Breadcrumb_Item_Post::setup_title()

---
+++ tests/unit-tests/Carbon_Breadcrumb_Item_Post/CarbonBreadcrumbItemPostSetupTitleTest.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @group item
+ */
+class CarbonBreadcrumbItemPostSetupTitleTest extends WP_UnitTestCase {
+
+	public function setUp() {
+		$this->item = $this->getMockForAbstractClass('Carbon_Breadcrumb_Item_Post', array(), '', false);
+		$this->post = $this->factory->post->create();
+		$this->item->set_id( $this->post );
+
+	}
+
+	public function tearDown() {
+		unset( $this->item );
+		unset( $this->post );
+	}
+
+	/**
+	 * @covers Carbon_Breadcrumb_Item_Post::setup_title
+	 */
+	public function testItemTitle() {
+		$this->assertSame( null, $this->item->get_title() );
+
+		$this->item->setup_title();
+
+		$title = apply_filters( 'the_title', get_post_field( 'post_title', $this->post ) );
+		$this->assertSame( $title, $this->item->get_title() );
+	}
+
+}
