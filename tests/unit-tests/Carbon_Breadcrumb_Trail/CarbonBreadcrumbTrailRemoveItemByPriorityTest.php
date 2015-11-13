422ef406a843324ba30aebc09e2b438964a47f26
Marin Atanasov <tyxla@abv.bg>
2015-11-13 22:23:54 +0200

    tests for Carbon_Breadcrumb_Trail::remove_item_by_priority()

---
+++ tests/unit-tests/Carbon_Breadcrumb_Trail/CarbonBreadcrumbTrailRemoveItemByPriorityTest.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * @group trail
+ */
+class CarbonBreadcrumbTrailRemoveItemByPriorityTest extends WP_UnitTestCase {
+
+	public function setUp() {
+		$this->trail = $this->getMock('Carbon_Breadcrumb_Trail', null);
+		$this->item = $this->getMockForAbstractClass('Carbon_Breadcrumb_Item');
+	}
+
+	public function tearDown() {
+		unset( $this->trail );
+		unset( $this->item );
+	}
+
+	/**
+	 * @covers Carbon_Breadcrumb_Trail::remove_item_by_priority
+	 */
+	public function testWithDefaultPriority() {
+		$original = array( 
+			5 => array( $this->item ), 
+			0 => array( $this->item ), 
+			1000 => array( $this->item ),
+		);
+		$this->trail->set_items( $original );
+
+		$this->trail->remove_item_by_priority();
+
+		$expected = array( 
+			5 => array( $this->item ), 
+			1000 => array( $this->item ) 
+		);
+		$this->assertSame( $expected, $this->trail->get_items() );
+	}
+
+	/**
+	 * @covers Carbon_Breadcrumb_Trail::remove_item_by_priority
+	 */
+	public function testWithCustomPriority() {
+		$original = array( 
+			5 => array( $this->item ), 
+			0 => array( $this->item ), 
+			1000 => array( $this->item ),
+		);
+		$this->trail->set_items( $original );
+
+		$this->trail->remove_item_by_priority( 5 );
+
+		$expected = array( 
+			0 => array( $this->item ), 
+			1000 => array( $this->item ) 
+		);
+		$this->assertSame( $expected, $this->trail->get_items() );
+	}
+
+}
