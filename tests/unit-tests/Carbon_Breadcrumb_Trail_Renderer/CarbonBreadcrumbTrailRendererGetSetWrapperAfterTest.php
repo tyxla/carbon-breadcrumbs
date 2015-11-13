e96f741c2f0635bd257e24ae24aaa3a1b518500e
Marin Atanasov <tyxla@abv.bg>
2015-11-13 23:00:40 +0200

    general tests for Carbon_Breadcrumb_Trail_Renderer setters & getters

---
+++ tests/unit-tests/Carbon_Breadcrumb_Trail_Renderer/CarbonBreadcrumbTrailRendererGetSetWrapperAfterTest.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @group trail_renderer
+ */
+class CarbonBreadcrumbTrailRendererGetSetWrapperAfterTest extends WP_UnitTestCase {
+
+	public function setUp() {
+		$this->renderer = $this->getMock('Carbon_Breadcrumb_Trail_Renderer', null);
+	}
+
+	public function tearDown() {
+		unset( $this->renderer );
+	}
+
+	/**
+	 * @covers Carbon_Breadcrumb_Trail_Renderer::get_wrapper_after
+	 * @covers Carbon_Breadcrumb_Trail_Renderer::set_wrapper_after
+	 */
+	public function testGetSetRenderer() {
+		$wrapper_after = 'foobar';
+		$this->renderer->set_wrapper_after( $wrapper_after );
+		$this->assertSame( $wrapper_after, $this->renderer->get_wrapper_after() );
+	}
+
+}
