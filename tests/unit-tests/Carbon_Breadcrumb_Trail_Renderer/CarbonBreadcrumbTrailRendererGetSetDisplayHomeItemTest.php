cd81df2e4d6fb6679647dc16b59c650eee5c9d98
Marin Atanasov <tyxla@abv.bg>
2015-11-16 22:24:48 +0200

    some more general tests for Carbon_Breadcrumb_Trail_Renderer

---
+++ tests/unit-tests/Carbon_Breadcrumb_Trail_Renderer/CarbonBreadcrumbTrailRendererGetSetDisplayHomeItemTest.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @group trail_renderer
+ */
+class CarbonBreadcrumbTrailRendererGetSetDisplayHomeItemTest extends WP_UnitTestCase {
+
+	public function setUp() {
+		$this->trail_renderer = $this->getMockForAbstractClass( 'Carbon_Breadcrumb_Trail_Renderer' );
+	}
+
+	public function tearDown() {
+		unset( $this->trail_renderer );
+	}
+
+	/**
+	 * @covers Carbon_Breadcrumb_Trail_Renderer::get_display_home_item
+	 * @covers Carbon_Breadcrumb_Trail_Renderer::set_display_home_item
+	 */
+	public function testNonBool() {
+		$this->trail_renderer->set_display_home_item( 0 );
+		$this->assertSame( false, $this->trail_renderer->get_display_home_item() );
+
+		$this->trail_renderer->set_display_home_item( "" );
+		$this->assertSame( false, $this->trail_renderer->get_display_home_item() );
+
+		$this->trail_renderer->set_display_home_item( 1 );
+		$this->assertSame( true, $this->trail_renderer->get_display_home_item() );
+
+		$this->trail_renderer->set_display_home_item( "foo" );
+		$this->assertSame( true, $this->trail_renderer->get_display_home_item() );
+	}
+
+}
---
+++ tests/unit-tests/Carbon_Breadcrumb_Trail_Renderer/CarbonBreadcrumbTrailRendererGetSetLastItemLinkTest.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @group trail_renderer
+ */
+class CarbonBreadcrumbTrailRendererGetSetLastItemLinkTest extends WP_UnitTestCase {
+
+	public function setUp() {
+		$this->trail_renderer = $this->getMockForAbstractClass( 'Carbon_Breadcrumb_Trail_Renderer' );
+	}
+
+	public function tearDown() {
+		unset( $this->trail_renderer );
+	}
+
+	/**
+	 * @covers Carbon_Breadcrumb_Trail_Renderer::get_last_item_link
+	 * @covers Carbon_Breadcrumb_Trail_Renderer::set_last_item_link
+	 */
+	public function testNonBool() {
+		$this->trail_renderer->set_last_item_link( 0 );
+		$this->assertSame( false, $this->trail_renderer->get_last_item_link() );
+
+		$this->trail_renderer->set_last_item_link( "" );
+		$this->assertSame( false, $this->trail_renderer->get_last_item_link() );
+
+		$this->trail_renderer->set_last_item_link( 1 );
+		$this->assertSame( true, $this->trail_renderer->get_last_item_link() );
+
+		$this->trail_renderer->set_last_item_link( "foo" );
+		$this->assertSame( true, $this->trail_renderer->get_last_item_link() );
+	}
+
+}
