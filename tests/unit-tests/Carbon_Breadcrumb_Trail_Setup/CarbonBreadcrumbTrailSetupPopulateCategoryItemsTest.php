e7d63da800131b901c47668dfc758ec7aa67f886
Marin Atanasov <tyxla@abv.bg>
2015-11-28 22:18:13 +0200

    tests for Carbon_Breadcrumb_Trail_Setup::populate_category_items()

---
+++ tests/unit-tests/Carbon_Breadcrumb_Trail_Setup/CarbonBreadcrumbTrailSetupPopulateCategoryItemsTest.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * @group trail_setup
+ */
+class CarbonBreadcrumbTrailSetupPopulateCategoryItemsTest extends WP_UnitTestCase {
+
+	public function setUp() {
+		parent::setUp();
+
+		$this->trail = $this->getMock('Carbon_Breadcrumb_Trail', null);
+		$this->setup = $this->getMock('Carbon_Breadcrumb_Trail_Setup', null, array(), '', false);
+		$this->setup->set_trail( $this->trail );
+		$this->parent_cat = $this->factory->category->create();
+		$this->child_cat = $this->factory->category->create(array(
+			'parent' => $this->parent_cat,
+		));
+		$this->post = $this->factory->post->create(array(
+			'post_category' => array( $this->parent_cat, $this->child_cat )
+		));
+	}
+
+	public function tearDown() {
+		unset( $this->trail );
+		unset( $this->setup );
+		unset( $this->post );
+		unset( $this->parent_cat );
+		unset( $this->child_cat );
+
+		parent::tearDown();
+	}
+
+	/**
+	 * @covers Carbon_Breadcrumb_Trail_Setup::populate_category_items
+	 */
+	public function testOnCategoryPage() {
+		$this->go_to('/?cat=' . $this->parent_cat );
+		$this->setup->populate_category_items();
+		$this->assertSame( array(), $this->trail->get_items() );
+
+		$this->go_to('/?cat=' . $this->child_cat );
+		$this->setup->populate_category_items();
+		$this->assertSame( array(), $this->trail->get_items() );
+	}
+
+	/**
+	 * @covers Carbon_Breadcrumb_Trail_Setup::populate_category_items
+	 */
+	public function testOnSinglePost() {
+		$this->go_to('/?p=' . $this->post );
+		$this->setup->populate_category_items();
+
+		$actual_items = array_values($this->trail->get_items());
+		$actual_items = $actual_items[0];
+		
+		$this->assertSame( 2, count( $actual_items ) );
+
+		$this->assertSame( $this->parent_cat, $actual_items[0]->get_id() );
+		$this->assertSame( 'term', $actual_items[0]->get_type() );
+		$this->assertSame( 'category', $actual_items[0]->get_subtype() );
+		$this->assertSame( 'Carbon_Breadcrumb_Item_Term', get_class($actual_items[0]) );
+		$this->assertSame( 700, $actual_items[0]->get_priority() );
+
+		$this->assertSame( $this->child_cat, $actual_items[1]->get_id() );
+		$this->assertSame( 'term', $actual_items[1]->get_type() );
+		$this->assertSame( 'category', $actual_items[1]->get_subtype() );
+		$this->assertSame( 'Carbon_Breadcrumb_Item_Term', get_class($actual_items[1]) );
+		$this->assertSame( 700, $actual_items[1]->get_priority() );
+	}
+
+}
