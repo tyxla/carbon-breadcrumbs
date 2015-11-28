93c42a5b8ba020ca754828e593ccdd9ddc7e15f4
Marin Atanasov <tyxla@abv.bg>
2015-11-28 22:40:48 +0200

    additional tests for Carbon_Breadcrumb_Item_Term::setup()

--- tests/unit-tests/Carbon_Breadcrumb_Item_Term/CarbonBreadcrumbItemTermSetupTest.php
+++ tests/unit-tests/Carbon_Breadcrumb_Item_Term/CarbonBreadcrumbItemTermSetupTest.php
@@ -5,11 +5,15 @@
 class CarbonBreadcrumbItemTermSetupTest extends WP_UnitTestCase {
 
 	public function setUp() {
+		parent::setUp();
+
 		$this->item = $this->getMockForAbstractClass('Carbon_Breadcrumb_Item_Term');
 	}
 
 	public function tearDown() {
 		unset( $this->item );
+
+		parent::tearDown();
 	}
 
 	/**
@@ -29,6 +33,23 @@ class CarbonBreadcrumbItemTermSetupTest extends WP_UnitTestCase {
 	public function testSetupWithoutSubtype() {
 		$this->item->set_id(123);
 		$this->item->setup();
+	}
+
+	/**
+	 * @covers Carbon_Breadcrumb_Item_Term::setup
+	 */
+	public function testSetupWithTerm() {
+		$term = $this->factory->category->create();
+		$this->item->set_id($term);
+		$this->item->set_subtype('category');
+
+		$this->item->setup();
+
+		$term_object = get_term_by( 'id', $term, 'category' );
+
+		$this->assertSame( $term_object->term_id, $this->item->term_object->term_id );
+		$this->assertSame( $term_object->term_taxonomy_id, $this->item->term_object->term_taxonomy_id );
+		$this->assertSame( $term_object->taxonomy, $this->item->term_object->taxonomy );
 	}
 
 }
