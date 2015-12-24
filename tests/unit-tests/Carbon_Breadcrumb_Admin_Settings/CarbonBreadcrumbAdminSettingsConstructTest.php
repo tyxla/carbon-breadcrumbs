bc5d98d4473fd1f5764eb71368dd7c371072b190
Marin Atanasov <tyxla@abv.bg>
2015-12-24 18:46:41 +0200

    tests for Carbon_Breadcrumb_Admin_Settings::__construct()

---
+++ tests/unit-tests/Carbon_Breadcrumb_Admin_Settings/CarbonBreadcrumbAdminSettingsConstructTest.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @group admin_settings
+ */
+class CarbonBreadcrumbAdminSettingsConstructTest extends WP_UnitTestCase {
+
+	public function setUp() {
+		$this->adminSettings = $this->getMock('Carbon_Breadcrumb_Admin_Settings', null, array(), '', false);
+	}
+
+	public function tearDown() {
+		unset( $this->adminSettings );
+	}
+
+	/**
+	 * @covers Carbon_Breadcrumb_Admin_Settings::__construct
+	 */
+	public function testHookedMethods() {
+		$this->adminSettings->__construct();
+		
+		$this->assertSame( 30, has_action( 'admin_menu', array($this->adminSettings, 'admin_menu') ) );
+		$this->assertSame( 10, has_action( 'admin_init', array($this->adminSettings, 'register_settings') ) );
+	}
+
+}
