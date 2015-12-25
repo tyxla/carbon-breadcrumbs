027cc2130e371ba2d669c67ff074b026a0399d52
Marin Atanasov <tyxla@abv.bg>
2015-12-25 20:32:50 +0200

    tests for Carbon_Breadcrumb_Admin_Settings_Field ID and title setters and getters

---
+++ tests/unit-tests/Carbon_Breadcrumb_Admin_Settings_Field/CarbonBreadcrumbAdminSettingsFieldGetSetIdTest.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @group admin_settings_field
+ */
+class CarbonBreadcrumbAdminSettingsFieldGetSetIdTest extends WP_UnitTestCase {
+
+	public function setUp() {
+		$this->adminField = $this->getMockForAbstractClass('Carbon_Breadcrumb_Admin_Settings_Field', array(123, 'test') );
+	}
+
+	public function tearDown() {
+		unset( $this->adminField );
+	}
+
+	/**
+	 * @covers Carbon_Breadcrumb_Admin_Settings_Field::get_id
+	 * @covers Carbon_Breadcrumb_Admin_Settings_Field::set_id
+	 */
+	public function testGetSetId() {
+		$expected = 123;
+		$this->adminField->set_id( $expected );
+		$this->assertSame( $expected, $this->adminField->get_id() );
+	}
+
+}
