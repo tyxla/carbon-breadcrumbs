e09b3f69b7b9c637f1864b388caf0ac7df6b9d13
Marin Atanasov <tyxla@abv.bg>
2015-12-29 19:40:41 +0200

    tests for Carbon_Breadcrumb_Admin_Settings::register_settings()

---
+++ tests/unit-tests/Carbon_Breadcrumb_Admin_Settings/CarbonBreadcrumbAdminSettingsRegisterSettingsTest.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * @group admin_settings
+ */
+class CarbonBreadcrumbAdminSettingsRegisterSettingsTest extends WP_UnitTestCase {
+
+	public function setUp() {
+		$this->adminSettings = $this->getMock('Carbon_Breadcrumb_Admin_Settings', null, array(), '', false);
+	}
+
+	public function tearDown() {
+		unset( $this->adminSettings );
+	}
+
+	/**
+	 * @covers Carbon_Breadcrumb_Admin_Settings::register_settings
+	 */
+	public function testRegisteredFields() {
+		$this->adminSettings->register_settings();
+
+		$fieldData = Carbon_Breadcrumb_Admin_Settings::get_field_data();
+
+		$this->assertSame( count($fieldData), count( $this->adminSettings->fields ) );
+
+		$key = 0;
+		foreach ($fieldData as $id => $field) {
+			$this->assertSame( $field['title'], $this->adminSettings->fields[$key]->get_title() );
+			$this->assertSame( 'carbon_breadcrumbs_' . $id, $this->adminSettings->fields[$key]->get_id() );
+			$this->assertInstanceOf( 'Carbon_Breadcrumb_Admin_Settings_Field_' . $field['type'], $this->adminSettings->fields[$key] );
+			$key++;
+		}
+	}
+
+}
