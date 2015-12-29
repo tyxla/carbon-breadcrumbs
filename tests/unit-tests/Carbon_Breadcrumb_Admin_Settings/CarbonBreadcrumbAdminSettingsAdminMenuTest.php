ab8ff1c1781b695b73a22e157357552a7e6fc455
Marin Atanasov <tyxla@abv.bg>
2015-12-29 19:28:59 +0200

    tests for Carbon_Breadcrumb_Admin_Settings::admin_menu

---
+++ tests/unit-tests/Carbon_Breadcrumb_Admin_Settings/CarbonBreadcrumbAdminSettingsAdminMenuTest.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * @group admin_settings
+ */
+class CarbonBreadcrumbAdminSettingsAdminMenuTest extends WP_UnitTestCase {
+
+	public function setUp() {
+		$this->adminSettings = $this->getMock('Carbon_Breadcrumb_Admin_Settings', null, array(), '', false);
+	}
+
+	public function tearDown() {
+		unset( $this->adminSettings );
+	}
+
+	/**
+	 * @covers Carbon_Breadcrumb_Admin_Settings::admin_menu
+	 */
+	public function testSettingsPageRegistered() {
+		global $_registered_pages, $submenu;
+
+		$page_title = Carbon_Breadcrumb_Admin_Settings::get_page_title();
+		$page_name = Carbon_Breadcrumb_Admin_Settings::get_page_name();
+
+		$current_user = get_current_user_id();
+		wp_set_current_user( self::factory()->user->create( array( 'role' => 'administrator' ) ) );
+
+		$this->adminSettings->admin_menu();
+		
+		$this->assertArrayHasKey( 'admin_page_' . $page_name, $_registered_pages );
+		$this->assertTrue( $_registered_pages['admin_page_' . $page_name] );
+
+		$submenuItem = array(
+			$page_title,
+			'manage_options',
+			$page_name,
+			$page_title
+		);
+		$this->assertArrayHasKey( 'options-general.php', $submenu );
+		$this->assertContains( $submenuItem, $submenu['options-general.php'] );
+	}
+
+	/**
+	 * @covers Carbon_Breadcrumb_Admin_Settings::admin_menu
+	 */
+	public function testSettingsSectionRegistered() {
+		global $wp_settings_sections;
+
+		$page_name = Carbon_Breadcrumb_Admin_Settings::get_page_name();
+
+		$this->adminSettings->admin_menu();
+
+		$sectionItem = array(
+			'id' => $page_name,
+			'title' => 'General Settings',
+			'callback' => ''
+		);
+		$this->assertArrayHasKey( $page_name, $wp_settings_sections );
+		$this->assertArrayHasKey( $page_name, $wp_settings_sections[$page_name] );
+		$this->assertSame( $sectionItem, $wp_settings_sections[$page_name][$page_name] );
+	}
+
+}
