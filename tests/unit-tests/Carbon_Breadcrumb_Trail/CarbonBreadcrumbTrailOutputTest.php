9e55eb65048a0e5b50df48ce747fd23e6b265ab8
Marin Atanasov <tyxla@abv.bg>
2015-12-29 12:04:21 +0200

    tests for Carbon_Breadcrumb_Trail::output()

---
+++ tests/unit-tests/Carbon_Breadcrumb_Trail/CarbonBreadcrumbTrailOutputTest.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * @group trail
+ */
+class CarbonBreadcrumbTrailOutputTest extends WP_UnitTestCase {
+
+	public function setUp() {
+		$this->trail = $this->getMock('Carbon_Breadcrumb_Trail', null);
+
+		$this->item1 = $this->getMockForAbstractClass('Carbon_Breadcrumb_Item');
+		$this->item2 = $this->getMockForAbstractClass('Carbon_Breadcrumb_Item');
+
+		$this->item1->set_link( '#1' );
+		$this->item2->set_link( '#2' );
+
+		$this->item1->set_title( 'Item 1' );
+		$this->item2->set_title( 'Item 2' );
+
+		$this->trail->add_item(array(
+			$this->item1,
+			$this->item2,
+		));
+	}
+
+	public function tearDown() {
+		unset( $this->trail );
+		unset( $this->item1 );
+		unset( $this->item2 );
+	}
+
+	/**
+	 * @covers Carbon_Breadcrumb_Trail::output
+	 */
+	public function testDefaultArgs() {
+		ob_start();
+		$trail = $this->getMock('Carbon_Breadcrumb_Trail', null);
+		$trail->setup();
+		$trail->render();
+		$expected = ob_get_clean();
+
+		ob_start();
+		Carbon_Breadcrumb_Trail::output();
+		$actual = ob_get_clean();
+
+		$this->assertSame( $expected, $actual );
+	}
+
+	/**
+	 * @covers Carbon_Breadcrumb_Trail::output
+	 */
+	public function testCustomArgs() {
+		$args = array(
+			'glue' => ' => ',
+		);
+
+		ob_start();
+		$trail = $this->getMock('Carbon_Breadcrumb_Trail', null, array($args));
+		$trail->setup();
+		$trail->render();
+		$expected = ob_get_clean();
+
+		ob_start();
+		Carbon_Breadcrumb_Trail::output( $args );
+		$actual = ob_get_clean();
+
+		$this->assertSame( $expected, $actual );
+	}
+
+}
