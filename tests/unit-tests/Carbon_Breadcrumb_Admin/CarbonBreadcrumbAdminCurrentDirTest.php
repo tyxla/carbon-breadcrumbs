9c12f6c8e43093677476499bd847ca0dfae85290
Marin Atanasov <tyxla@abv.bg>
2016-01-06 01:26:27 +0200

    tests for Carbon_Breadcrumb_Admin::current_dir()

---
+++ tests/unit-tests/Carbon_Breadcrumb_Admin/CarbonBreadcrumbAdminCurrentDirTest.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @group admin
+ */
+class CarbonBreadcrumbAdminCurrentDirTest extends WP_UnitTestCase {
+
+	public function setUp() {
+		$this->admin = $this->getMock('Carbon_Breadcrumb_Admin', null, array(), '', false);
+	}
+
+	public function tearDown() {
+		unset( $this->admin );
+	}
+
+	/**
+	 * @covers Carbon_Breadcrumb_Admin::current_dir
+	 */
+	public function testCurrentDir() {
+		$expected = dirname( dirname( dirname( dirname( __FILE__ ) ) ) ) . DIRECTORY_SEPARATOR . 'admin';
+		$actual = $this->admin->current_dir();
+		$this->assertSame( $expected, $actual );
+	}
+
+}
